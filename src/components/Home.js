5b1a78810c9425377aaaea30c220ace27ca676ef
lilkgb <mcleanrs@comcast.net>
2019-05-02 16:42:50 -0700

    add fun animation on home component to test key frames

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,17 @@ import styled from 'styled-components'
 function Home(){
   const Something = styled.h1`
   @keyframes pulse {
-    0%{color: green}
-    50%{color: red}
-    100%{color: green}
+    0%{color: hotpink}
+    50%{color: limegreen}
+    100%{color: hotpink}
   }
     animation: pulse 1s linear infinite;
     color: green;
+    &:hover {
+      cursor: pointer;
+      animation: none;
+      color: black;
+    }
   `
   return(
     <div>
